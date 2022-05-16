8c9549d6390e1dfc72221fc9c8dcdf238bb150dc
Maria Gomez <maria_gg86@yahoo.es>
2022-05-16 18:57:01 +0200

    Prioritizing the Testcases with TestNG

---
+++ src/test/Day3.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Day3 {
+
+    @Test
+    public void WebLoginCarLoan() {
+
+        //Selenium
+        System.out.println("WebLoginCar");
+    }
+
+    @Test
+    public void MobileLoginCarLoan() {
+
+        //Appium
+        System.out.println("MobileLoginCar");
+    }
+
+    @Test
+    public void loginAPICarLoan() {
+
+        //Rest API automation
+        System.out.println("APILoginCar");
+    }
+}
---
+++ src/test/Day4.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Day4 {
+
+    @Test
+    public void WebLoginHomeLoan() {
+
+        //Selenium
+        System.out.println("WebLoginHome");
+    }
+
+    @Test
+    public void MobileLoginHomeLoan() {
+
+        //Appium
+        System.out.println("MobileLoginHome");
+    }
+
+    @Test
+    public void loginAPIHomeLoan() {
+
+        //Rest API automation
+        System.out.println("APILoginHome");
+    }
+}
