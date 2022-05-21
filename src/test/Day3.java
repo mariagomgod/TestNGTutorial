88a29c5915a11323d68f0f9345fa2d4f387ec115
Maria Gomez <maria_gg86@yahoo.es>
2022-05-21 15:52:40 +0200

    Data provider annotation

--- src/test/Day3.java
+++ src/test/Day3.java
@@ -1,12 +1,11 @@
 package test;
 
-import com.beust.jcommander.Parameter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -46,11 +45,13 @@ public class Day3 {
         System.out.println("WebLoginSigInCar");
     }
 
-    @Test(timeOut = 4000)
-    public void WebLoginSigOutCarLoan() {
+    @Test(dataProvider = "getData")
+    public void WebLoginSigOutCarLoan(String username, String password) {
 
         //Selenium
         System.out.println("WebLoginSigOutCar");
+        System.out.println(username);
+        System.out.println(password);
     }
 
     @Test(groups = {"Smoke"})
@@ -77,5 +78,23 @@ public class Day3 {
     public void afterClass() {
 
         System.out.println("After executing all methods in the class");
+    }
+
+    @DataProvider
+    public Object[] getData() {
+
+        // 1st combination - username password - good credit history -row
+        // 2nd combination - username password - no credit history
+        // 3rd combination - fraudulent credit history
+        Object [][] data = new Object[3][2];
+        data [0][0] = "firstSetUsername";
+        data [0][1] = "firstPassword";
+
+        data [1][0] = "secondSetUsername";
+        data [1][1] = "secondPassword";
+
+        data [2][0] = "thirdSetUsername";
+        data [2][1] = "thirdPassword";
+        return new Object[0];
     }
 }
